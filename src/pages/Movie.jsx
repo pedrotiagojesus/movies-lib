6425b1849dea08ce607d29774d1e6424c21ccde3
Pedro Jesus <pedrotiagojesus@gmail.com>
2024-05-17 12:36:41 +0100

    Put the movie crew and cast in slide

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -5,10 +5,15 @@ import {
     BsWallet2,
     BsHourglassSplit,
     BsFillFileEarmarkTextFill,
+    BsChevronLeft,
+    BsChevronRight,
 } from "react-icons/bs";
-import { useTmdbGet } from "../hooks/useTmdbGet";
+
+import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
 import "./Movie.css";
+import "../Splide.css";
 
 const moviesUrl = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
@@ -52,8 +57,40 @@ const Movie = () => {
         getMovieCredit(apiUrl);
     }, []);
 
+    const sliderOptions = {
+        gap: "1rem",
+        pagination: false,
+        mediaQuery: "min",
+        breakpoints: {
+            0: {
+                perPage: 2,
+                perMove: 1,
+            },
+            576: {
+                perPage: 3,
+                perMove: 1,
+            },
+            768: {
+                perPage: 5,
+                perMove: 1,
+            },
+            992: {
+                perPage: 6,
+                perMove: 3,
+            },
+            1200: {
+                perPage: 6,
+                perMove: 3,
+            },
+            1400: {
+                perPage: 6,
+                perMove: 3,
+            },
+        },
+    };
+
     return (
-        <div className="container movie-page">
+        <div id="movie-page" className="container">
             {movie && (
                 <>
                     <div className="row">
@@ -65,31 +102,33 @@ const Movie = () => {
                             />
                         </div>
                         <div className="col-md-8">
-                            <h2>{movie.title}</h2>
+                            <h2 className="title">{movie.title}</h2>
                             <p className="tagline">{movie.tagline}</p>
                             <div className="list-info">
                                 <div className="info">
                                     <h3>
-                                        <BsWallet2 /> Budget
+                                        <BsWallet2 className="icon" /> Budget
                                     </h3>
                                     <p>{formatCurrency(movie.budget)}</p>
                                 </div>
                                 <div className="info">
                                     <h3>
-                                        <BsGraphUp /> Revenue
+                                        <BsGraphUp className="icon" /> Revenue
                                     </h3>
                                     <p>{formatCurrency(movie.revenue)}</p>
                                 </div>
                                 <div className="info">
                                     <h3>
-                                        <BsHourglassSplit /> Runtime
+                                        <BsHourglassSplit className="icon" />{" "}
+                                        Runtime
                                     </h3>
                                     <p>{movie.runtime} minutes</p>
                                 </div>
                             </div>
                             <div className="info description">
                                 <h3>
-                                    <BsFillFileEarmarkTextFill /> Overview
+                                    <BsFillFileEarmarkTextFill className="icon" />{" "}
+                                    Overview
                                 </h3>
                                 <p>{movie.overview} </p>
                             </div>
@@ -98,62 +137,120 @@ const Movie = () => {
                 </>
             )}
             {movieCredit && (
-                <>
+                <div className="credit">
                     {movieCredit.cast && (
                         <>
                             <h2 className="title">Cast</h2>
-                            <div className="row credit-container">
-                                {movieCredit.cast.map((cast) => (
-                                    <div key={cast.id} className="col-md-2">
-                                        <div className="card credit-card">
-                                            <div
-                                                className="ratio img-wrapper"
-                                                style={{
-                                                    backgroundImage: `url(${imageUrl}${cast.profile_path})`,
-                                                }}
-                                            ></div>
-                                            <div className="card-body">
-                                                <h4 className="card-title">
-                                                    {cast.character}
-                                                </h4>
-                                                <h5 className="card-subtitle">
-                                                    {cast.name}
-                                                </h5>
+                            <Splide
+                                aria-label="Cast"
+                                className="credit-container"
+                                options={sliderOptions}
+                                hasTrack={false}
+                            >
+                                <SplideTrack>
+                                    {movieCredit.cast.map((cast, i) => (
+                                        <SplideSlide key={`cast-${i}`}>
+                                            <div className="card credit-card">
+                                                <div
+                                                    className="ratio img-wrapper"
+                                                    style={{
+                                                        backgroundImage: `url(${imageUrl}${cast.profile_path})`,
+                                                    }}
+                                                ></div>
+                                                <div className="card-body">
+                                                    <h4
+                                                        className="card-title"
+                                                        title={cast.character}
+                                                    >
+                                                        {cast.character}
+                                                    </h4>
+                                                    <h5 className="card-subtitle">
+                                                        {cast.name}
+                                                    </h5>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </div>
-                                ))}
-                            </div>
+                                        </SplideSlide>
+                                    ))}
+                                </SplideTrack>
+
+                                <div className="splide__arrows">
+                                    <button
+                                        className="splide__arrow splide__arrow--prev"
+                                        type="button"
+                                        aria-label="Previous slide"
+                                        aria-controls="splide01-track"
+                                    >
+                                        <BsChevronRight />
+                                    </button>
+                                    <button
+                                        className="splide__arrow splide__arrow--next"
+                                        type="button"
+                                        aria-label="Next slide"
+                                        aria-controls="splide01-track"
+                                    >
+                                        <BsChevronRight />
+                                    </button>
+                                </div>
+                            </Splide>
                         </>
                     )}
                     {movieCredit.crew && (
                         <>
                             <h2 className="title">Crew</h2>
-                            <div className="row credit-container">
-                                {movieCredit.crew.map((crew) => (
-                                    <div key={crew.id} className="col-md-2">
-                                        <div className="card credit-card">
-                                            <div
-                                                className="ratio img-wrapper"
-                                                style={{
-                                                    backgroundImage: `url(${imageUrl}${crew.profile_path})`,
-                                                }}
-                                            ></div>
-                                            <div className="card-body">
-                                                <h4 className="card-title">
-                                                    {crew.name}
-                                                </h4>
-                                                <h5 className="card-subtitle">
-                                                    {crew.department}
-                                                </h5>
+                            <Splide
+                                aria-label="Crew"
+                                className="credit-container"
+                                options={sliderOptions}
+                                hasTrack={false}
+                            >
+                                <SplideTrack>
+                                    {movieCredit.crew.map((crew, i) => (
+                                        <SplideSlide key={`crew-${i}`}>
+                                            <div className="card credit-card">
+                                                <div
+                                                    className="ratio img-wrapper"
+                                                    style={{
+                                                        backgroundImage: `url(${imageUrl}${crew.profile_path})`,
+                                                    }}
+                                                ></div>
+                                                <div className="card-body">
+                                                    <h4
+                                                        className="card-title"
+                                                        title={crew.name}
+                                                    >
+                                                        {crew.name}
+                                                    </h4>
+                                                    <h5 className="card-subtitle">
+                                                        {crew.department}
+                                                    </h5>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </div>
-                                ))}
-                            </div>
+                                        </SplideSlide>
+                                    ))}
+                                </SplideTrack>
+
+                                <div className="splide__arrows">
+                                    <button
+                                        className="splide__arrow splide__arrow--prev"
+                                        type="button"
+                                        aria-label="Previous slide"
+                                        aria-controls="splide01-track"
+                                    >
+                                        <BsChevronRight />
+                                    </button>
+                                    <button
+                                        className="splide__arrow splide__arrow--next"
+                                        type="button"
+                                        aria-label="Next slide"
+                                        aria-controls="splide01-track"
+                                    >
+                                        <BsChevronRight />
+                                    </button>
+                                </div>
+                            </Splide>
                         </>
                     )}
-                </>
+                </div>
             )}
         </div>
     );
