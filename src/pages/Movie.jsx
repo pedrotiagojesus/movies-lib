25269733a49790f4866a05ac86cabe841caa94ab
Pedro Jesus <pedrotiagojesus1995@gmail.com>
2024-05-29 16:47:09 +0100

    Add list of images and videos to the movies.

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -25,6 +25,8 @@ const Movie = () => {
     const { id } = useParams();
     const [movie, setMovie] = useState(null);
     const [movieCredit, setMovieCredit] = useState(null);
+    const [movieImage, setMovieImage] = useState(null);
+    const [movieVideo, setMovieVideo] = useState([]);
 
     const formatCurrency = (number) => {
         return number.toLocaleString("en-US", {
@@ -41,12 +43,7 @@ const Movie = () => {
         setMovie(data);
     };
 
-    useEffect(() => {
-        const apiUrl = `${moviesUrl}${id}?${apiKey}`;
-        getMovie(apiUrl);
-    }, []);
-
-    // Get Movie Crdits
+    // Get Movie Credits
     const getMovieCredit = async (apiUrl) => {
         const res = await fetch(apiUrl);
         const data = await res.json();
@@ -54,9 +51,48 @@ const Movie = () => {
         setMovieCredit(data);
     };
 
+    // Get Movie Images
+    const getMovieImage = async (apiUrl) => {
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+
+        setMovieImage(data.backdrops);
+    };
+
+    // Get Movie Video
+    const getMovieVideo = async (apiUrl) => {
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+
+        const allowType = ["Trailer", "Teaser"];
+
+        setMovieVideo([]);
+
+        data.results.map((video) => {
+            if (!video.official) {
+                return;
+            }
+
+            if (!allowType.includes(video.type)) {
+                return;
+            }
+
+            setMovieVideo((prevArray) => [video, ...prevArray]);
+        });
+    };
+
     useEffect(() => {
-        const apiUrl = `${moviesUrl}${id}/credits?${apiKey}`;
-        getMovieCredit(apiUrl);
+        const movieUrl = `${moviesUrl}${id}?${apiKey}`;
+        getMovie(movieUrl);
+
+        const creditUrl = `${moviesUrl}${id}/credits?${apiKey}`;
+        getMovieCredit(creditUrl);
+
+        const imageUrl = `${moviesUrl}${id}/images?${apiKey}&language=en`;
+        getMovieImage(imageUrl);
+
+        const videoUrl = `${moviesUrl}${id}/videos?${apiKey}&language=en`;
+        getMovieVideo(videoUrl);
     }, []);
 
     const sliderOptions = {
@@ -89,6 +125,84 @@ const Movie = () => {
                 perMove: 3,
             },
         },
+    };
+
+    const sliderImageOptions = {
+        gap: "1rem",
+        pagination: false,
+        mediaQuery: "min",
+        breakpoints: {
+            0: {
+                perPage: 1,
+                perMove: 1,
+            },
+            576: {
+                perPage: 2,
+                perMove: 1,
+            },
+            768: {
+                perPage: 3,
+                perMove: 1,
+            },
+            992: {
+                perPage: 4,
+                perMove: 3,
+            },
+            1200: {
+                perPage: 4,
+                perMove: 3,
+            },
+            1400: {
+                perPage: 4,
+                perMove: 3,
+            },
+        },
+    };
+
+    const sliderVideoOptions = {
+        gap: "1rem",
+        pagination: false,
+        mediaQuery: "min",
+        breakpoints: {
+            0: {
+                perPage: 1,
+                perMove: 1,
+            },
+            576: {
+                perPage: 2,
+                perMove: 1,
+            },
+            768: {
+                perPage: 2,
+                perMove: 1,
+            },
+            992: {
+                perPage: 3,
+                perMove: 3,
+            },
+            1200: {
+                perPage: 4,
+                perMove: 4,
+            },
+            1400: {
+                perPage: 4,
+                perMove: 4,
+            },
+        },
+    };
+
+    const renderVideo = (video) => {
+        return (
+            <iframe
+                src={`https://www.youtube.com/embed/${video.key}`}
+                title={video.title}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+                className="mw-100"
+            ></iframe>
+        );
     };
 
     return (
@@ -151,94 +265,172 @@ const Movie = () => {
                     </div>
                 </>
             )}
-            {movieCredit && (
-                <div className="credit">
-                    {movieCredit.cast && (
-                        <>
-                            <h2 className="title">Cast</h2>
-                            <Splide
-                                aria-label="Cast"
-                                className="credit-container"
-                                options={sliderOptions}
-                                hasTrack={false}
-                            >
-                                <SplideTrack>
-                                    {movieCredit.cast.map((cast, i) => (
-                                        <SplideSlide key={`cast-${i}`}>
-                                            <PersonCard
-                                                image={cast.profile_path}
-                                                title={cast.character}
-                                                subtitle={cast.name}
-                                            />
-                                        </SplideSlide>
-                                    ))}
-                                </SplideTrack>
+            <div className="movie-details">
+                {movieImage && (
+                    <div className="image">
+                        <h2 className="title">Images</h2>
+                        <Splide
+                            aria-label="Images"
+                            className="image-container"
+                            options={sliderImageOptions}
+                            hasTrack={false}
+                        >
+                            <SplideTrack>
+                                {movieImage.map((image, i) => (
+                                    <SplideSlide key={`image-${i}`}>
+                                        <img
+                                            src={`${imageUrl}${image.file_path}`}
+                                            alt={`image-${i}`}
+                                            className="img-fluid"
+                                        />
+                                    </SplideSlide>
+                                ))}
+                            </SplideTrack>
 
-                                <div className="splide__arrows">
-                                    <button
-                                        className="splide__arrow splide__arrow--prev"
-                                        type="button"
-                                        aria-label="Previous slide"
-                                        aria-controls="splide01-track"
-                                    >
-                                        <BsChevronRight />
-                                    </button>
-                                    <button
-                                        className="splide__arrow splide__arrow--next"
-                                        type="button"
-                                        aria-label="Next slide"
-                                        aria-controls="splide01-track"
-                                    >
-                                        <BsChevronRight />
-                                    </button>
-                                </div>
-                            </Splide>
-                        </>
-                    )}
-                    {movieCredit.crew && (
-                        <>
-                            <h2 className="title">Crew</h2>
-                            <Splide
-                                aria-label="Crew"
-                                className="credit-container"
-                                options={sliderOptions}
-                                hasTrack={false}
-                            >
-                                <SplideTrack>
-                                    {movieCredit.crew.map((crew, i) => (
-                                        <SplideSlide key={`crew-${i}`}>
-                                            <PersonCard
-                                                image={crew.profile_path}
-                                                title={crew.name}
-                                                subtitle={crew.department}
-                                            />
-                                        </SplideSlide>
-                                    ))}
-                                </SplideTrack>
+                            <div className="splide__arrows">
+                                <button
+                                    className="splide__arrow splide__arrow--prev"
+                                    type="button"
+                                    aria-label="Previous slide"
+                                    aria-controls="splide01-track"
+                                >
+                                    <BsChevronRight />
+                                </button>
+                                <button
+                                    className="splide__arrow splide__arrow--next"
+                                    type="button"
+                                    aria-label="Next slide"
+                                    aria-controls="splide01-track"
+                                >
+                                    <BsChevronRight />
+                                </button>
+                            </div>
+                        </Splide>
+                    </div>
+                )}
+                {movieVideo && (
+                    <div className="video">
+                        <h2 className="title">Videos</h2>
+                        <Splide
+                            aria-label="Video"
+                            className="video-container"
+                            options={sliderVideoOptions}
+                            hasTrack={false}
+                        >
+                            <SplideTrack>
+                                {movieVideo.map((video, i) => (
+                                    <SplideSlide key={`video-${i}`}>
+                                        {renderVideo(video)}
+                                    </SplideSlide>
+                                ))}
+                            </SplideTrack>
 
-                                <div className="splide__arrows">
-                                    <button
-                                        className="splide__arrow splide__arrow--prev"
-                                        type="button"
-                                        aria-label="Previous slide"
-                                        aria-controls="splide01-track"
-                                    >
-                                        <BsChevronRight />
-                                    </button>
-                                    <button
-                                        className="splide__arrow splide__arrow--next"
-                                        type="button"
-                                        aria-label="Next slide"
-                                        aria-controls="splide01-track"
-                                    >
-                                        <BsChevronRight />
-                                    </button>
-                                </div>
-                            </Splide>
-                        </>
-                    )}
-                </div>
-            )}
+                            <div className="splide__arrows">
+                                <button
+                                    className="splide__arrow splide__arrow--prev"
+                                    type="button"
+                                    aria-label="Previous slide"
+                                    aria-controls="splide01-track"
+                                >
+                                    <BsChevronRight />
+                                </button>
+                                <button
+                                    className="splide__arrow splide__arrow--next"
+                                    type="button"
+                                    aria-label="Next slide"
+                                    aria-controls="splide01-track"
+                                >
+                                    <BsChevronRight />
+                                </button>
+                            </div>
+                        </Splide>
+                    </div>
+                )}
+                {movieCredit && movieCredit.cast && (
+                    <div className="movie-cast">
+                        <h2 className="title">Cast</h2>
+                        <Splide
+                            aria-label="Cast"
+                            className="credit-container"
+                            options={sliderOptions}
+                            hasTrack={false}
+                        >
+                            <SplideTrack>
+                                {movieCredit.cast.map((cast, i) => (
+                                    <SplideSlide key={`cast-${i}`}>
+                                        <PersonCard
+                                            image={cast.profile_path}
+                                            title={cast.character}
+                                            subtitle={cast.name}
+                                        />
+                                    </SplideSlide>
+                                ))}
+                            </SplideTrack>
+
+                            <div className="splide__arrows">
+                                <button
+                                    className="splide__arrow splide__arrow--prev"
+                                    type="button"
+                                    aria-label="Previous slide"
+                                    aria-controls="splide01-track"
+                                >
+                                    <BsChevronRight />
+                                </button>
+                                <button
+                                    className="splide__arrow splide__arrow--next"
+                                    type="button"
+                                    aria-label="Next slide"
+                                    aria-controls="splide01-track"
+                                >
+                                    <BsChevronRight />
+                                </button>
+                            </div>
+                        </Splide>
+                    </div>
+                )}
+                {movieCredit && movieCredit.crew && (
+                    <div className="movie-crew">
+                        <h2 className="title">Crew</h2>
+                        <Splide
+                            aria-label="Crew"
+                            className="credit-container"
+                            options={sliderOptions}
+                            hasTrack={false}
+                        >
+                            <SplideTrack>
+                                {movieCredit.crew.map((crew, i) => (
+                                    <SplideSlide key={`crew-${i}`}>
+                                        <PersonCard
+                                            image={crew.profile_path}
+                                            title={crew.name}
+                                            subtitle={crew.department}
+                                        />
+                                    </SplideSlide>
+                                ))}
+                            </SplideTrack>
+
+                            <div className="splide__arrows">
+                                <button
+                                    className="splide__arrow splide__arrow--prev"
+                                    type="button"
+                                    aria-label="Previous slide"
+                                    aria-controls="splide01-track"
+                                >
+                                    <BsChevronRight />
+                                </button>
+                                <button
+                                    className="splide__arrow splide__arrow--next"
+                                    type="button"
+                                    aria-label="Next slide"
+                                    aria-controls="splide01-track"
+                                >
+                                    <BsChevronRight />
+                                </button>
+                            </div>
+                        </Splide>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
